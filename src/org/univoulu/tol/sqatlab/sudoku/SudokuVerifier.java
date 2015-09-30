7da729430dbd831ee228628538caf991683f8742
somename <someemail>
2015-09-30 16:10:41 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -31,7 +31,8 @@ public class SudokuVerifier {
 		int depth = 9;
 		for(int i=0;i<9;i++){
 			for(int j=0;j<9;j++){
-				System.out.println("i"+String.valueOf(i)+"j"+String.valueOf(j));
+				System.out.println("i"+String.valueOf(i)+"j"+String.valueOf(j) +
+						"idx"+String.valueOf(i+(j*depth)));
 
 				if(tempArray.contains(arr [i + (j*depth)] ) ){
 					return false;
