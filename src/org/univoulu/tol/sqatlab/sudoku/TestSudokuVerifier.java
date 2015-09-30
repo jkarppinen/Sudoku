78bac57568299883c77ad90f6ee2288c53f7e40b
somename <someemail>
2015-09-30 16:42:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -54,7 +54,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void test_invalid_subgrid() {
 		SudokuVerifier s = new SudokuVerifier();
-		String testValue = "444369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		String testValue = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 		assertEquals(-2, s.verify(testValue));
 	}
 	
