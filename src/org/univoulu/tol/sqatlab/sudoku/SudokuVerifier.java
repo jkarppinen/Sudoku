d13f9a34d95d3f797a162b267e3a53ef0c2d6b45
somename <someemail>
2015-09-30 16:27:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -11,7 +11,7 @@ public class SudokuVerifier {
 			return -1;
 		}
 		if(! digitsOnly(candidateSolution)) {
-			return -1;
+			return -3;
 		}
 		if(! validSubGrids(candidateSolution)){
 			return -2;
