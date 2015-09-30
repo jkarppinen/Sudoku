52de07a9e4a40ef5affa35aabc69c4eb94ebbe37
somename <someemail>
2015-09-30 16:41:35 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -36,7 +36,7 @@ public class SudokuVerifier {
 
 			List<String> tempArray = new ArrayList<String>();
 			for(int j=0;j<depth;j++){
-				String letter = arr[i + j * 9];
+				String letter = arr[i + j * depth];
 				//System.out.println(String.valueOf(i+j*9) + " Val: " + letter);
 				if(tempArray.contains(letter)){
 					return false;
