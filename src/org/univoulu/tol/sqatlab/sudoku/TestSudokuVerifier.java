dd0b5ee30ae6f2223ffa246d5db89de1761a0e61
somename <someemail>
2015-09-30 15:42:54 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -25,6 +25,14 @@ public class TestSudokuVerifier {
 		String testValue = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 		assertEquals(-1, s.verify(testValue));
 	}
+
+	
+	@Test
+	public void test_alphanumerics() {
+		SudokuVerifier s = new SudokuVerifier();
+		String testValue = "A17369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals(-1, s.verify(testValue));
+	}
 	
 	
 
